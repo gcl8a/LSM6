145a7cb10d517e9da2a994c07d4f6cf62f1c6beb
Greg Lewin <gcl8a@icloud.com>
2023-04-23 17:00:17 -0400

    public conversions

--- src/LSM6.h
+++ src/LSM6.h
@@ -145,6 +145,7 @@ public:
 
     int16_t testReg(uint8_t address, regAddr reg);
 
+public:
     //conversion factors are set when you change ODR or FS
     float mdpsPerLSB = 0;
     float mgPerLSB = 0;
